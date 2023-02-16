510c7adaa639c0482bae8e392193d174e2863233
pym <pym66@outlook.com>
2023-02-16 18:10:36 +0800

    Initial commit

---
+++ src/main/java/com/example/waibao1/WaiBao1Application.java
@@ -0,0 +1,13 @@
+package com.example.waibao1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiBao1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaiBao1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/waibao1/WaiBao1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waibao1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaiBao1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
